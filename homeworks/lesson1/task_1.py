5e0ea7bfff922369e98a3d718467fcf90e05c234
Tetiana Koltunova <tatyana.koltunova.tk@gmail.com>
2026-06-11 10:15:54 +0300

    task_1 done

--- homeworks/lesson1/task_1.py
+++ homeworks/lesson1/task_1.py
@@ -1,11 +1,11 @@
 # Створіть 6 змінних з будь-якими значенням
 
-var_1 = 'asdhgasj1'
-var_2 = "asdasd"
-var_3 = 123
-var_4 = 234
-var_5 = True
-var_6 = False
+my_var_1_str = 'my_string'
+my_var_2_str = "my_string_2"
+my_var_3_int = 12
+my_var_4_int = 100
+my_var_5_float = 3.14
+my_var_6_bool = False
 
 # Робота з числами
 # Умова:
@@ -14,19 +14,26 @@ var_6 = False
 # Обчисліть їх суму, різницю і тд.
 # Виведіть результати кожної операції на екран з відповідними поясненнями.
 
-a = 100
-b = 20
+a = 12
+b = 100.1
 
-sum = a + b
-print(sum)
-minus = a - b
-print(minus)
+add = a + b
+print(f"{a} plus {b} equals:", add)
+
+deduct = b - a
+print(f"{b} minus {a} equals:", deduct)
+
 multiply = a * b
-print(multiply)
-divide = a / b
-print(divide)
-int_divide = a // b
-print(int_divide)
+print(f"{a} multiplied by {b} equals:", multiply)
+
+divide = b / a
+print(f"{b} divided by {a} equals:", divide)
+
+int_divide = b // a
+print(f"integer division: {b} // {a} equals:", int_divide)
+
+mod = b % a
+print(f"modulus division: {b} % {a} equals:", mod)
 
 # Робота з рядками (Strings)
 # Умова:
@@ -35,9 +42,11 @@ print(int_divide)
 # Створіть змінну last_name та присвойте їй ваше прізвище.
 # Створіть змінну full_name, об'єднавши (конкатенувавши) ім'я та прізвище через пробіл.
 # Виведіть full_name на екран.
-first_name = "Sergii"
-last_name = "Miguel"
-full_name = first_name + " " + last_name
 
+first_name = "Tetiana"
+last_name = "Koltunova"
+full_name = first_name + " " + last_name
+full_name_2 = f"{first_name} {last_name}"
 print(full_name)
+print("My full name is", full_name_2)
 
